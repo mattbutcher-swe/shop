73a107b19677b07b7f3310757421d887683045cb
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-02 18:47:21 -0400

    setup docker

---
+++ frontend/src/AddRecipe.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Add Recipe
+      <form>
+        Name
+        <input type="text" name="name"></input>
+        Ingredients
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Recipes
+      <button>Add</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Order</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
