129bebb0ef7441a0feddc296ba2b87c698825fe3
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-04 16:35:58 -0400

    added java backend

---
+++ backend/src/main/java/github/com/mattbutcher_swe/shop_backend/Test.java
@@ -0,0 +1,21 @@
+package github.com.mattbutcher_swe.shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Test {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Test.class, args);
+  }
+
+}
---
+++ backend/src/test/java/github/com/mattbutcher_swe/shop_backend/ShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.mattbutcher_swe.shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
