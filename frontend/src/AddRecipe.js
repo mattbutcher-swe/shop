6d2cf405ab2e7dccbd6bff103605a44f48286d5f
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-08 08:28:30 -0400

    WIP - building out add recipe form

--- frontend/src/AddRecipe.js
+++ frontend/src/AddRecipe.js
@@ -4,13 +4,37 @@ function AddRecipe() {
   return (
     <div className="App">
       Add Recipe
-      <form>
-        Name
-        <input type="text" name="name"></input>
-        Ingredients
-      </form>
+        <form>
+          <div className="form-group row">
+            <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Name</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="name"/>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Description</label>
+            <div className="col-sm-10">
+              <input type="password" className="form-control" id="description"/>
+            </div>
+          </div>
+          <div className="form-group row">
+            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Ingredients</label>
+            <div className="col-sm-10 d-flex justify-content-end">
+              <button type="button" onClick={appendNewIngredientInput} className="btn btn-primary">+</button>            
+            </div>
+          </div>
+          <div id='ingredients'>
+          </div>
+        </form>
     </div>
   );
+}
+
+function appendNewIngredientInput() {
+  const ingredientsDiv = document.getElementById('ingredients');
+  const test = document.createElement('p');
+  test.innerHTML = "test";
+  ingredientsDiv.appendChild(test);
 }
 
 export default AddRecipe;
