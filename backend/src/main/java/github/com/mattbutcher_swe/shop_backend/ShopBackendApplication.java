77ee334f9314d6d9c5cd5df0479aeeb8f87e9976
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-08 07:05:29 -0400

    renamed main methods class to more generic ShopBackendApplication

--- backend/src/main/java/github/com/mattbutcher_swe/shop_backend/ShopBackendApplication.java
+++ backend/src/main/java/github/com/mattbutcher_swe/shop_backend/ShopBackendApplication.java
@@ -2,21 +2,14 @@ package github.com.mattbutcher_swe.shop_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Test {
-
-  @RequestMapping("/")
-  public String home() {
-    int test = 5;
-    return "Hello World123";
-  }
+public class ShopBackendApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Test.class, args);
+    SpringApplication.run(ShopBackendApplication.class, args);
   }
 
 }
