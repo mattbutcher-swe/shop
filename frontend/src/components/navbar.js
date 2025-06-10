f12da45d36fc57bc0978dcddbb284455e91a64d5
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-10 12:00:36 -0400

    fixed navbar expand bug

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,18 +1,20 @@
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-lg">
-                <a class="navbar-brand" href="#">Shop</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <a className="navbar-brand" href="#">Shop</a>
+                <button className="navbar-toggler" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/recipes">Meals</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/recipes">Meals</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/pantry">Pantry</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/pantry">Pantry</a>
                         </li>
                     </ul>
                 </div>
