3d806357f325bfbfc382c7a2bf4be03262b377b7
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-12 16:10:09 -0400

    WIP - setting up meal order form

--- frontend/src/components/RecipeTile.js
+++ frontend/src/components/RecipeTile.js
@@ -4,21 +4,23 @@ import './RecipeTile.css'
 import '../App.css';
 
 const RecipeTile = ({ recipe }) => {
+    const updateOrderStatus = (e) => {
+        const tile = e.currentTarget;
+        tile.classList.toggle('ordered');
+    }
+
     return (
-        <div className='recipe-tile v-stack-fill h-auto'>
+        <div className='recipe-tile v-stack-fill h-auto' onClick={(e) => updateOrderStatus(e)}>
             <div className="v-grow-scroll d-flex align-items-center justify-content-center">
-                    {recipe.name}
+                {recipe.name}
             </div>
-            <div className="d-flex">
-                    <span className='w-50 text-center'>
-                        <Link to={"/recipes/edit/" + recipe.id}>
-                              Edit
-                        </Link>
-                    </span>
-                    <span className='w-50 text-center'>Order</span>
+            <div className="d-flex justify-content-center">
+                <Link to={"/recipes/edit/" + recipe.id}>
+                    Edit
+                </Link>
             </div>
         </div>
-        
+
     );
 };
 
