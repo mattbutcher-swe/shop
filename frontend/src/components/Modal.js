2c9e42de5e9e2f8837277d77fdb8426e3c9871ca
mattbutcher-swe <mattbutcher.swe@gmail.com>
2025-06-22 18:08:43 -0400

    improved modal UX

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './Modal.css'; // style it how you want
+import './Modal.css';
 
 const Modal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="container-half modal-overlay v-stack-fill">
-            <div className='d-flex bg-white p-2'>
-                <button className="btn btn-light ms-auto" onClick={onClose}>×</button>
+        <div className='modal-background'>
+            <div className="container-half modal-overlay v-stack-fill">
+                <div className='d-flex bg-white p-2'>
+                    <button className="btn btn-light ms-auto" onClick={onClose}>×</button>
+                </div>
+                {children}
             </div>
-            {children}
         </div>
     );
 };
